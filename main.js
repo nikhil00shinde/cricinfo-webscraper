11cd6886d317969e12f76cc175867fa196f9a490
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-29 10:58:44 +0530

    scrapping data completed

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import request from "request";
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
 // Venue Date Opponent result runs balls fours sixes strikerate
 
+import { getAllMatchesLink } from "./Allmatch.js";
+
 //home page
 request(url, cb);
 
@@ -21,27 +23,4 @@ function extractLink(html) {
 	let link = anchorElem.attr("href");
 	let fullLink = "https://www.espncricinfo.com/" + link;
 	getAllMatchesLink(fullLink);
-}
-
-function getAllMatchesLink(url) {
-	request(url, function (err, response, html) {
-		if (err) {
-			console.log(err);
-		} else {
-			extractAllLinks(html);
-		}
-	});
-}
-
-function extractAllLinks(html) {
-	let $ = cheerio.load(html);
-
-	let scorecardElems = $("a[data-hover='Scorecard']");
-
-	for (let i = 0; i < scorecardElems.length; i++) {
-		let link = $(scorecardElems[i]).attr("href");
-
-		let fullLink = "https://www.espncricinfo.com/" + link;
-		console.log(fullLink);
-	}
 }
--- scorecard.js
+++ scorecard.js
@@ -1,12 +1,14 @@
 import cheerio from "cheerio";
 import request from "request";
 
-const url =
-	"https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
+// const url =
+// 	"https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
 // Venue Date Opponent result runs balls fours sixes strikerate
 
 //home page
-request(url, cb);
+function processScorecard(url) {
+	request(url, cb);
+}
 
 function cb(err, response, html) {
 	if (err) {
@@ -32,7 +34,41 @@ function extractMatchDetails(html) {
 	let stringArr = descElem.split(",");
 	let venue = stringArr[1].trim();
 	let date = stringArr[2].trim();
-	console.log(venue);
-	console.log(date);
-	console.log(result);
+
+	let innings = $(".card.content-block.match-scorecard-table > .Collapsible ");
+	let htmlString = "";
+	for (let i = 0; i < innings.length; i++) {
+		// htmlString += $(innings[i]).html();
+		//team opponent
+		let teamName = $(innings[i]).find("h5").text();
+		teamName = teamName.split("INNINGS")[0].trim();
+		let opponentIndex = i == 0 ? 1 : 0;
+
+		let opponentName = $(innings[opponentIndex]).find("h5").text();
+		opponentName = opponentName.split("INNINGS")[0].trim();
+		// console.log(teamName);
+		// console.log(opponentName);
+
+		let cInnings = $(innings[i]);
+		let allRows = cInnings.find(".table.batsman tbody tr");
+		for (let j = 0; j < allRows.length; j++) {
+			let allCols = $(allRows[j]).find("td");
+			let isWorthy = $(allCols[0]).hasClass("batsman-cell");
+
+			if (isWorthy == true) {
+				// players runs balls fours sixes strikerate
+				let playerName = $(allCols[0]).text().trim();
+				let runs = $(allCols[2]).text().trim();
+				let balls = $(allCols[3]).text().trim();
+				let fours = $(allCols[5]).text().trim();
+				let sixes = $(allCols[6]).text().trim();
+				let sr = $(allCols[7]).text().trim();
+
+				console.log(`${playerName} ${runs} ${balls} ${fours} ${sixes} ${sr}`);
+			}
+		}
+	}
+	// console.log(htmlString);
 }
+
+export { processScorecard };
